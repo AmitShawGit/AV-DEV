769eb52f68d9cd3e2f424fd7e43e93e7c4565a7e
Amit <amitshaw1421@gmail.com>
2023-11-28 22:45:49 +0530

    fine tuning all completed

--- js/owl.js
+++ js/owl.js
@@ -2,6 +2,9 @@ $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
     nav:true,
+    autoplay:true,
+    autoplayTimeout:2000,
+    navText:["<i class='fa-solid fa-angle-left'></i>","<i class='fa-solid fa-angle-right'></i>"],
     responsive:{
         0:{
             items:1
@@ -12,5 +15,6 @@ $('.owl-carousel').owlCarousel({
         1000:{
             items:3
         }
-    }
+    },
+   
 })
